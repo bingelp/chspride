109e220ce4e0f085275d96829eaaa6e33ff943b2
Pete Bingel <peter.bingel@charlestonpride.org>
2018-04-15 09:16:35 -0400

    Added working slider however re-downloads image every tick

--- src/components/Sponsors/SponsorHero.js
+++ src/components/Sponsors/SponsorHero.js
@@ -9,7 +9,8 @@ export default class SponsorHero extends React.Component {
         super(props);
         this.state = {
             sponsors: [],
-            isLoaded: false
+            isLoaded: false,
+            timerID: null
         }
     }
 
@@ -25,6 +26,20 @@ export default class SponsorHero extends React.Component {
                 })
             })
         });
+        this.timerID = setInterval(
+            () => this.tick(),
+            3000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+      }
+
+    tick(){
+        let [first, ...rest] = this.state.sponsors;
+        let sponsors = [...rest, first];
+        this.setState({sponsors: sponsors});
     }
 
     render() {
@@ -33,10 +48,11 @@ export default class SponsorHero extends React.Component {
                 <div className="container text-center">
                     <h2>Thank You to Our Sponsors</h2>
                     <div className="row">
-                        {isLoaded && sponsors.map(s => <Sponsor key={s.id} name={s.details.name} level={s.details.level} url={s.details.url} logo={s.details.logo} description={s.details.description} />)}
+                        {isLoaded && sponsors.slice(0,6).map(s => <Sponsor key={s.id} name={s.details.name} level={s.details.level} url={s.details.url} logo={s.details.logo} description={s.details.description} />)}
                     </div>
                     <Link to="/sponsorships" className="btn btn-success btn-raised" > <i className="zmdi zmdi-thumb-up"></i>Become a sponsor</Link>
                     <Link to="/sponsors" className="btn btn-info btn-raised" > <i className="zmdi zmdi-eye"></i>View All Sponsors</Link>
+                    <button className="btn btn-warning btn-raised" onClick={() => this.tick()}> Tick</button>
                 </div>)
     }
 }
