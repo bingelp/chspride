957306c637366712df00afe89133c0e1ca884390
Pete Bingel <peter.bingel@charlestonpride.org>
2018-04-17 22:43:05 -0400

    Wired up Upcoming Events with firebase

--- src/components/Events/UpcomingEvent.js
+++ src/components/Events/UpcomingEvent.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 class UpcomingEvent extends React.Component {
-    render() {
+    render(props) {
     return (
     <div className="col-lg-3">
        <div className="ms-thumbnail-container">
         <figure className="ms-thumbnail ms-thumbnail-light">
-          <img src="http://via.placeholder.com/300x400" alt="" className="img-fluid"></img>
+          <img src={this.props.meta.graphicPath} alt={this.props.meta.name} className="img-fluid"></img>
           <figcaption className="ms-thumbnail-caption text-center">
             <div className="ms-thumbnail-caption-content">
-              <h3 className="ms-thumbnail-caption-title color-royal">Caption Title</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              <a href="javascript:void(0)" className="btn-circle btn-circle-raised btn-circle-sm ml-1 mr-1 btn-facebook">
+              <h3 className="ms-thumbnail-caption-title color-royal">{this.props.meta.name}</h3>
+                <h4>{this.props.meta.location}</h4>
+              <p>{this.props.meta.date}</p>
+              <a href={this.props.meta.url} className="btn-circle btn-circle-raised btn-circle-sm ml-1 mr-1 btn-facebook">
                 <i className="zmdi zmdi-facebook"></i>
               </a>
             </div>
--- src/components/Events/UpcomingEvents.js
+++ src/components/Events/UpcomingEvents.js
@@ -1,22 +1,55 @@
 import React from 'react';
-import Event from './UpcomingEvent'
-import { Link } from 'react-router-dom';
+import Event from './UpcomingEvent';
+import firebase from "../../firebase";
+
 
 class UpcomingEvents extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            events: []
+        }
+    }
+
+    componentDidMount() {
+        const itemsRef = firebase.database().ref('events').orderByKey();
+        itemsRef.once('value', (snapshot) => {
+            snapshot.forEach(childSnapShot => {
+                let event = {id: childSnapShot.key, meta: childSnapShot.val()};
+                let storageRef = firebase.storage().ref();
+                let graphicRef = storageRef.child('events/' + event.meta.graphic);
+                graphicRef.getDownloadURL().then(url => {
+                        event.meta.graphicPath = url;
+                        this.state.events.push(event);
+                        this.setState({
+                            events: this.state.events
+                        })
+                    }
+                ).catch(error => {
+                    this.setState(
+                        {
+                            error: "Error: " + error.code
+                        }
+                    )
+                })
+
+            })
+        });
+    }
+
     render() {
-    return (
-        <div className="wrap bg-primary color-dark">
-        <div className="container text-center">
-            <h2>Upcoming Events</h2>
-            <div className="row">
-            <Event/>
-            <Event/>
-            <Event/>
-            <Event/>
-            </div>
-            <Link to="/events" className="btn btn-warning btn-raised" > <i className="zmdi zmdi-eye"></i>View All Events</Link>
-        </div>
-        </div>)
+        let events = this.state.events;
+        events.sort((s1,s2) => new Date(s1.meta.date) - new Date(s2.meta.date));
+        return (
+            <div className="wrap bg-primary color-dark">
+                <div className="container text-center">
+                    <h2>Upcoming Events</h2>
+                    <div className="row">
+                        {events.map(s => <Event key={s.id} meta={s.meta}/>)}
+                    </div>
+                </div>
+            </div>)
     }
 }
 
