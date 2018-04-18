3f16bc0f3b7fa53e8bade1cc8fa6b31f821914a5
Pete Bingel <peter.bingel@charlestonpride.org>
2018-04-17 23:58:03 -0400

    Addressed responsive issues on sm screens

--- src/components/Events/UpcomingEvent.js
+++ src/components/Events/UpcomingEvent.js
@@ -3,7 +3,7 @@ import React from 'react';
 class UpcomingEvent extends React.Component {
     render(props) {
     return (
-    <div className="col-lg-3">
+    <div className="col-sm-6 col-lg-3">
        <div className="ms-thumbnail-container">
         <figure className="ms-thumbnail ms-thumbnail-light">
           <img src={this.props.meta.graphicPath} alt={this.props.meta.name} className="img-fluid"></img>
@@ -11,7 +11,7 @@ class UpcomingEvent extends React.Component {
             <div className="ms-thumbnail-caption-content">
               <h3 className="ms-thumbnail-caption-title color-royal">{this.props.meta.name}</h3>
                 <h4>{this.props.meta.location}</h4>
-              <p>{this.props.meta.date}</p>
+              <h4>{this.props.meta.date}</h4>
               <a href={this.props.meta.url} className="btn-circle btn-circle-raised btn-circle-sm ml-1 mr-1 btn-facebook">
                 <i className="zmdi zmdi-facebook"></i>
               </a>
