07dfbb13ca844096b78913b8c19c64453aa42762
Pete Bingel <bingelp@gmail.com>
2019-03-22 16:43:37 -0400

    Updated Mailing List and Donate pages

--- src/pages/Donate/Donate.js
+++ src/pages/Donate/Donate.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import './Donate.css'
-const Donate = (props) => {
-    return (
-    <div>
-        <div className="ms-hero-page-override ms-hero-img-city ms-hero-bg-info">
-            <div className="container">
-            <div className="text-center">
-                <h1 className="no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5">Donate</h1>
-                <h2 className="no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5">Help Make Charleston Pride a Reality</h2>
-                <p className="lead lead-sm color-white text-center center-block mt-2 mw-800 fw-300 animated fadeInUp animation-delay-7">A lot of time, effort, and money goes into organizing and putting on Charleston Pride. Charleston Pride Festival is funded strictly through sponsors and funds raised at events. Due to this fact, we have to raise money throughout the year in order for Charleston Pride Festival to be successful.  If you cannot make it to one of our fundraisers, you can now donate directly through the Charleston Pride Festival site!</p>
-            </div>
-            </div>
-        </div>
-        <div className="container card-hero">
-            <div className="card">
-                <div className="card-body">
-                    <iframe id="etapIframe" title="donate" src="https://app.etapestry.com/onlineforms/CharlestonPrideFestivalInc/donate.html"></iframe>
-                    <script src="//app.etapestry.com/hosted/eTapestry.com/etapEmbedResponsiveResizing.js" type="text/JavaScript"></script>
-                </div>
-            </div>
-        </div>
-    </div>
-    )};
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Donate.css";
 
-export default Donate;
+export default class MailingList extends Component {
+  Header() {
+    var headerClass =
+      "no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5";
+    var heroClass =
+      "lead color-white text-center center-block mt-2 mw-800 fw-300 animated fadeInUp animation-delay-7";
+    return (
+      <div className="ms-hero-page-override ms-hero-img-donate ms-hero-bg-success">
+        <div className="container">
+          <div className="text-center">
+            <h1 className={headerClass}>Donate</h1>
+            <p className={heroClass}>Help Make Charleston Pride a Reality!</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  Registration() {
+    return (
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <h3>
+              A lot of time, effort, and money goes into organizing and putting
+              on <strong>Charleston Pride</strong>.
+              <strong> Charleston Pride Festival, Inc</strong> is funded
+              strictly through sponsors and funds raised at events. Due to this
+              fact, we have to raise money throughout the year in order for{" "}
+              <strong>Charleston Pride</strong> to be successful. If you cannot
+              make it to one of our fundraisers, you can now donate here! Also
+              check out our monthly donor program,{" "}
+              <Link to="/friends-of-pride">Friends of Pride</Link>.
+            </h3>
+            <iframe
+              id="etapIframe"
+              title="donate"
+              src="https://app.etapestry.com/onlineforms/CharlestonPrideFestivalInc/donate.html"
+            />
+            <script
+              src="//app.etapestry.com/hosted/eTapestry.com/etapEmbedResponsiveResizing.js"
+              type="text/JavaScript"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <this.Header />
+        <this.Registration />
+      </div>
+    );
+  }
+}
--- src/pages/MailingList/MailingList.js
+++ src/pages/MailingList/MailingList.js
@@ -1,29 +1,56 @@
-import React, { Component } from 'react'
-import ETap from '../../components/ETapestryForm'
+import React, { Component } from "react";
+import "./MailingList.css";
 
 export default class MailingList extends Component {
-  render() {
-      var headerClass = "no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5"
-      var heroClass = "lead lead-sm color-white text-center center-block mt-2 mw-800 fw-300 animated fadeInUp animation-delay-7"
-      return (
-        <div>
-            <div className="ms-hero-page-override ms-hero-img-city ms-hero-bg-info">
-                <div className="container">
-                    <div className="text-center">
-                        <h1 className={headerClass}>Join our Mailing List</h1>
-                        <h2 className={headerClass}>Be in the know about all things Charleston Pride!</h2>
-                        <p className={heroClass}>By filling out the form below you are signing up to receive our newsletter and announcements for upcoming events.</p>
-                    </div>
-                </div>
-            </div>
-            <div className="container card-hero">
-                <div className="card">
-                    <div className="card-body">
-                        <ETap title="Mailing List" form="emailsignup" />
-                    </div>
-                </div>
-            </div>
+  Header() {
+    var headerClass =
+      "no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5";
+    var heroClass =
+      "lead color-white text-center center-block mt-2 mw-800 fw-300 animated fadeInUp animation-delay-7";
+    return (
+      <div className="ms-hero-page-override ms-hero-img-mailing ms-hero-bg-primary">
+        <div className="container">
+          <div className="text-center">
+            <h1 className={headerClass}>Join our Mailing List</h1>
+            <p className={heroClass}>
+              Be in the know about all things Charleston Pride!
+            </p>
+          </div>
         </div>
-    )
+      </div>
+    );
+  }
+
+  Registration() {
+    return (
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <h3>
+              By filling out the form below you are signing up to receive our
+              newsletter and announcements for upcoming events.
+            </h3>
+            <iframe
+              id="etapIframe"
+              title="mailing-list"
+              src="https://app.etapestry.com/onlineforms/CharlestonPrideFestivalInc/emailsignup.html"
+            />
+            <script
+              src="//app.etapestry.com/hosted/eTapestry.com/etapEmbedResponsiveResizing.js"
+              type="text/JavaScript"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <this.Header />
+        <this.Registration />
+      </div>
+    );
   }
 }
