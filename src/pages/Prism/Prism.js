c28bc21cbced70c6a92e45081232561c0bf91693
Pete Bingel <bingelp@gmail.com>
2019-06-02 18:13:41 -0400

    Updated Prism for 2019

--- src/pages/Prism/Prism.js
+++ src/pages/Prism/Prism.js
@@ -1,41 +1,81 @@
-import React, { Component } from 'react';
-import { ExternalBtn, ExternalLink } from '../../components/LinkUtil'
-import { Link } from 'react-router-dom'
-import './Prism.css'
-import jade_jolie from './jade_jolie.jpg'
-import jaidynn_diore_fierce from './jaidynn_diore_fierce.jpg'
-import trinity_the_trick_taylor from './trinity_the_trick_taylor.jpg'
-import yuhua_hamasaki from './yuhua_hamasaki.jpg'
-import prism_logo from './prism-logo.jpg'
+import React, { Component } from "react";
+import { ExternalLink } from "../../components/LinkUtil";
+import "./Prism.css";
+import SocialList from "../../components/SocialList/SocialList";
+import { Link } from "react-router-dom";
+import ETapestryForm from "../../components/ETapestryForm";
 
 class Prism extends Component {
-
   Header(props) {
     return (
       <div className="ms-hero ms-hero-page-override ms-hero-img-prism ms-hero-bg-dark">
         <div className="container">
           <div className="text-center">
-            <h1 className="no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5">Prism Party</h1>
-            <h2 className="lead color-white text-center center-block mt-2 mw-800 fw-300 animated fadeInUp animation-delay-7">The Official After Party of Charleston Pride</h2>
-            <h3 className="lead color-white text-center center-block mt-2 mw-800 fw-300 animated fadeInUp animation-delay-7">Saturday, September 22 @ 9:00 PM - 1:30 AM | Music Farm</h3>
-            <h3 className="lead color-warning text-center center-block mt-2 mw-800 fw-300 animated fadeInUp animation-delay-7">$20 <span className="color-white">|</span> 18+</h3>
-            <ExternalBtn to="https://www.etix.com/ticket/p/4283681/prism-party-charleston-the-music-farm-charleston" icon="ticket" title="Tickets" color="warning" />
-            <p className="color-white">This event is <strong>Free</strong> for <Link to="/friends-of-pride">Friends of Pride</Link>. Email <a href="mailto:info@charlestonpride.org">info@charlestonpride.org</a> to reserve.</p>
+            <h1 className="no-m ms-site-title color-white center-block ms-site-title-lg mt-2 animated zoomInDown animation-delay-5">
+              <span className="initialism">
+                <span>P</span>
+                <span>r</span>
+                <span>i</span>
+                <span>s</span>
+                <span>m</span>
+              </span>
+              : <small>The Official Party of Charleston Pride</small>
+            </h1>
+            <h2 className="lead color-white text-center center-block mt-2 mw-800 fw-300 animated fadeInUp animation-delay-7">
+              Saturday, September 14 @ 7:00 PM - 11:00 PM
+            </h2>
+            <h2 className="lead color-white text-center center-block mt-2 mw-800 fw-300 animated fadeInUp animation-delay-7">
+              Charleston Bus Shed <span className="color-warning">&amp;</span>{" "}
+              Visitor’s Center
+            </h2>
           </div>
         </div>
       </div>
-    )
+    );
   }
 
+  Entertainer(props) {
+    return (
+      <div className={"card width-auto card-" + props.card + "-inverse"}>
+        <figure
+          className={
+            "ms-thumbnail ms-thumbnail-horizontal ms-thumbnail-" + props.card
+          }
+        >
+          <img src={props.img} alt={props.name} className="img-fluid" />
+          <figcaption className="ms-thumbnail-caption text-center">
+            <div className="ms-thumbnail-caption-content">
+              <h4 className="ms-thumbnail-caption-title mb-2">{props.name}</h4>
+              <SocialList
+                facebook={props.facebook}
+                twitter={props.twitter}
+                instagram={props.instagram}
+                web={props.web || ""}
+              />
+            </div>
+          </figcaption>
+        </figure>
+        <div className="text-center">
+          <h4>{props.name}</h4>
+        </div>
+      </div>
+    );
+  }
   Queen(props) {
     return (
-      <div className="col-lg-3 col-6 queen">
-        <div className="card">
-          <figure className="ms-thumbnail ms-thumbnail-horizontal">
+      <div className="col-lg-6 col-12 queen">
+        <div className={"card card-" + props.card + "-inverse"}>
+          <figure
+            className={
+              "ms-thumbnail ms-thumbnail-horizontal ms-thumbnail-" + props.card
+            }
+          >
             <img src={props.src} alt={props.name} className="img-fluid" />
             <figcaption className="ms-thumbnail-caption text-center">
               <div className="ms-thumbnail-caption-content">
-                <h4 className="ms-thumbnail-caption-title mb-2">{props.name}</h4>
+                <h4 className="ms-thumbnail-caption-title mb-2">
+                  {props.name}
+                </h4>
               </div>
             </figcaption>
           </figure>
@@ -44,38 +84,198 @@ class Prism extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
-  render() {
+
+  Dudleys() {
     return (
-      <div id="prism">
-        <this.Header />
-        <div className="container">
-          <div className="card">
-            <div className="row">
-              <div className="col-12 col-md-3 text-center">
-                <img src={prism_logo} alt="prism logo" className="img-fluid" />
-                <ExternalBtn to="https://www.etix.com/ticket/p/4283681/prism-party-charleston-the-music-farm-charleston" icon="ticket" title="Tickets" color="primary" />
-              </div>
-              <div className="col-12 col-md-9">
-                <h3 className="text-center">We painted the town every <span className="initialism"><span>c</span><span>o</span><span>l</span><span>o</span><span>r</span></span> of the rainbow this year. Now, it's time to show Charleston how beautiful life can be when those colors come together! You won't want to miss the Charleston Pride Prism Party as we close out a fantastic week of celebrating Pride.</h3>
-                <h3 className="text-center">The Prism Party will be hosted by <ExternalLink to="https://www.facebook.com/thepotluckclub" title="Patti O'Furniture" /> with <ExternalLink to="https://www.facebook.com/djtrevord/" title="DJ Trevor D" /> spinning the beats all night. We're proud to introduce the Queens of Charleston hour showcasing some of our amazing local girls to kickoff the night!</h3>
-              </div>
+      <div className="col-12 col-md-4 text-center">
+        <h2>Presented By:</h2>
+        <img
+          src="https://s3.amazonaws.com/charlestonpride.assets/chspride/prism/dudleys.jpg"
+          alt="dudleys"
+          className="img-fluid"
+          width="200px"
+        />
+      </div>
+    );
+  }
+
+  PriceTable() {
+    return (
+      <div class="row no-gutters">
+        <div class="col-lg-4">
+          <div class="price-table price-table-info wow zoomInUp animation-delay-2">
+            <header class="price-table-header">
+              <span class="price-table-category">General Admission</span>
+              <h3>
+                <sup>$</sup>15.00 <sub>Online</sub>
+                <br />
+                <sup>$</sup>20.00 <sub>At Door</sub>
+              </h3>
+            </header>
+            <div class="price-table-body">
+              <ul class="price-table-list">
+                <li>Access to the Party.</li>
+              </ul>
             </div>
-            <div className="row">
-              <h3 className="text-center col-12">Entertainment Featuring the Contestants of Ru Paul's Drag Race</h3>
-              <this.Queen name="Jade Jolie" src={jade_jolie} />
-              <this.Queen name="Jaidyn Diore Fierce" src={jaidynn_diore_fierce} />
-              <this.Queen name="Trinity Taylor" src={trinity_the_trick_taylor} />
-              <this.Queen name="Yuhua Hamasaki" src={yuhua_hamasaki} />
-              <h3 className="text-center col-12">Queens of Charleston</h3>
-              <h4 className="text-center col-12">Crystal Guysir &middot; Kymmya Starr &middot; Diamond Giavonni &middot; Melody Lucas &middot; Venus Alexander</h4>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="price-table price-table-success prominent wow zoomInDown animation-delay-2">
+            <header class="price-table-header">
+              <span class="price-table-category">VIP</span>
+              <h3>
+                <sup>$</sup>50.00 <sub />
+              </h3>
+              General Admission Plus
+            </header>
+            <div class="price-table-body">
+              <ul class="price-table-list">
+                <li>Meet and Greet with Todrick Hall and the Ru Paul Queens</li>
+                <li>
+                  Admission to Last Call at Music Farm - the official late night
+                  dance party of Charleston Pride
+                  <br />
+                  <small>(details coming soon)</small>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="price-table price-table-warning wow zoomInUp animation-delay-2">
+            <header class="price-table-header">
+              <span class="price-table-category">Friends of Pride</span>
+              <h3>Free</h3>
+              Same as VIP
+            </header>
+            <div class="price-table-body">
+              <ul class="price-table-list">
+                <li>
+                  As a <Link to="/friends"> Friend of Pride </Link>
+                  you get access to both Prism and Last Call. Reserve your
+                  ticket by emailing Megan at{" "}
+                  <a href="mailto:friends@charlestonpride.org">
+                    friends@charlestonpride.org
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
     );
   }
-}
 
+  Description(props) {
+    return (
+      <div className="col-12 col-md-8">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="text-center">
+                We painted the town every{" "}
+                <span className="initialism">
+                  <span>c</span>
+                  <span>o</span>
+                  <span>l</span>
+                  <span>o</span>
+                  <span>r</span>
+                </span>{" "}
+                of the rainbow this year. Now, it's time to show Charleston how
+                beautiful life can be when those colors come together! You won't
+                want to miss{" "}
+                <span className="initialism">
+                  <span>P</span>
+                  <span>r</span>
+                  <span>i</span>
+                  <span>s</span>
+                  <span>m</span>
+                </span>
+                : <em>The Official Party of Charleston Pride</em> as we close
+                out a fantastic week of celebrating Pride.
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  render() {
+    return (
+      <div id="prism">
+        <this.Header />
+        <div className="container">
+          <div class="card">
+            <div class="row">
+              <this.Dudleys />
+              <this.Description PriceTable={this.PriceTable} />
+            </div>
+          </div>
+          <div className="card">
+            <h1 class="text-center">Entertainment</h1>
+            <div className="card-body">
+              <div className="row text-center">
+                <div className="col-12 col-lg-6">
+                  <this.Entertainer
+                    name="Host - Brooke Collins"
+                    img="https://s3.amazonaws.com/charlestonpride.assets/chspride/prism/brooke.jpg"
+                    web=""
+                    facebook="brooke.collins.1800"
+                    instagram=""
+                    twitter=""
+                    card="danger"
+                  />
+                </div>
+                <div className="col-12 col-lg-6">
+                  <this.Entertainer
+                    name="DJ - Earl Newell Jr"
+                    img="https://s3.amazonaws.com/charlestonpride.assets/chspride/prism/earl.jpg"
+                    web="https://www.thedancedoctors.com/"
+                    facebook="scdancedoctors"
+                    instagram="quietkingz"
+                    twitter=""
+                    card="info"
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <h3 className="text-center col-12">
+                  Entertainment Featuring the Contestants of Ru Paul's Drag Race
+                </h3>
+                <this.Queen
+                  name="Alexis Mateo"
+                  src="https://s3.amazonaws.com/charlestonpride.assets/chspride/prism/AlexisMateo.jpg"
+                  card="success"
+                />
+                <this.Queen
+                  name="Mayhem Miller"
+                  src="https://s3.amazonaws.com/charlestonpride.assets/chspride/prism/mayhem.jpg"
+                  card="warning"
+                />
+                <this.Queen
+                  name="Vanessa Vanjie Mateo"
+                  src="https://s3.amazonaws.com/charlestonpride.assets/chspride/prism/vanjie.jpg"
+                  card="royal"
+                />
+                <this.Queen
+                  name="Queens of Charleston - Coming Soon"
+                  src="https://s3.amazonaws.com/charlestonpride.assets/chspride/prism/carmella.jpg"
+                  card="primary"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="card text-center">
+            <h1>Tickets</h1>
+            <p>This event is 18+</p>
+            <this.PriceTable />
+            <ETapestryForm form="prism" title="Prism" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default Prism;
