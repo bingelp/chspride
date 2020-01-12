22b27c6dfb88326e6538f8d407c41315ab6df843
Pete Bingel <bingelp@gmail.com>
2020-01-11 23:41:18 -0500

    Updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './Footer.css'
+import "./Footer.css";
 
 const Footer = () => (
   <footer className="ms-footer">
@@ -16,19 +16,37 @@ const Footer = () => (
           </h1>
         </div>
         <div className="col-md-6 col-lg-3 order-lg-last text-center">
-            <h4>Contact Us</h4>
-          <address className="text-center" >
-              <p><i className="color-info-light zmdi zmdi-email mr-1"></i><br/><a href="mailto:info@charlestonpride.org">info@charlestonpride.org</a></p>
-            <p><i className="color-danger-light zmdi zmdi-pin mr-1"></i><br/>PO Box 61558<br />North Charleston, SC 29419</p>
+          <h4>Contact Us</h4>
+          <address className="text-center">
+            <p>
+              <i className="color-info-light zmdi zmdi-email mr-1"></i>
+              <br />
+              <a href="mailto:info@charlestonpride.org">
+                info@charlestonpride.org
+              </a>
+            </p>
+            <p>
+              <i className="color-danger-light zmdi zmdi-pin mr-1"></i>
+              <br />
+              1801 Reynolds Ave. Unit A<br />
+              North Charleston, SC 29405
+            </p>
           </address>
         </div>
         <div className="col-lg-6">
           <div className="m-5">
-            <p>Copyright &copy; 2019 Charleston Pride Festival, Inc. All Rights Reserved.</p>
-            <p>Charleston Pride Festival, Inc. is a 501(c)(3) public charity under the Internal Revenue Service Code of 1986 and the State of South Carolina.</p>
+            <p>
+              Copyright &copy; 2020 Charleston Pride Festival, Inc. All Rights
+              Reserved.
+            </p>
+            <p>
+              Charleston Pride Festival, Inc. is a 501(c)(3) public charity
+              under the Internal Revenue Service Code of 1986 and the State of
+              South Carolina.
+            </p>
           </div>
         </div>
-        </div>
+      </div>
     </div>
   </footer>
 );
